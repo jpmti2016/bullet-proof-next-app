180e529c8c6833c983d06bce27709cc47e20a0e7
Yampier Medina <jpmti2016@gmail.com>
2020-12-18 23:50:40 -0500

    Nextjs blog with markdown files with embed Youtuve React component

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import Theme from "../components/Theme";
+import ms from "ms";
+
+import { promises as fsPromises } from "fs";
+
+export default function Home({ postList }) {
+  return (
+    <Theme>
+      <div className="post-list">
+        {postList.map((post) => (
+          <div key={post.slug} className="post-link">
+            <Link href="/post/[slug]" as={`/post/${post.slug}`}>
+              <a>
+                <div className="time">
+                  {ms(Date.now() - post.createdAt, { long: true })} ago
+                </div>
+                <div className="title">{post.title}</div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Theme>
+  );
+}
+
+export async function getStaticProps() {
+  const markdownFiles = await fsPromises.readdir("data");
+
+  const postList = markdownFiles.map((filename) => {
+    const slug = filename.replace(/.md$/, "");
+    const [year, month, day, ...rest] = slug.split("-");
+    const createdAt = new Date(`${year} ${month} ${day}`).getTime();
+    const title = rest.join(" ");
+
+    return {
+      slug,
+      title,
+      createdAt,
+    };
+  });
+
+  return {
+    props: {
+      postList,
+    },
+  };
+}
---
+++ pages/post/[slug].js
@@ -0,0 +1,60 @@
+import Theme from "../../components/Theme";
+import Youtube from "../../components/Youtuve";
+import ms from "ms";
+import { promises as fsPromises } from "fs";
+import Markdown from "markdown-to-jsx";
+
+export default function Post({ post }) {
+  return (
+    <Theme>
+      <div className="post">
+        <div className="time">
+          Published {ms(Date.now() - post.createdAt, { long: true })} ago
+        </div>
+        <h1>{post.title}</h1>
+        <div className="content">
+          <Markdown
+            options={{ overrides: { Youtube: { component: Youtube } } }}
+          >
+            {post.content}
+          </Markdown>
+        </div>
+      </div>
+    </Theme>
+  );
+}
+
+export async function getStaticPaths() {
+  const markdownFiles = await fsPromises.readdir("data");
+
+  const paths = markdownFiles.map((filename) => {
+    const slug = filename.replace(/.md$/, "");
+    return {
+      params: { slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const [year, month, day, ...rest] = params.slug.split("-");
+  const createdAt = new Date(`${year} ${month} ${day}`).getTime();
+  const title = rest.join(" ");
+
+  const content = await fsPromises.readFile(`data/${params.slug}.md`, "utf-8");
+
+  return {
+    props: {
+      post: {
+        slug: params.slug,
+        title,
+        content,
+        createdAt,
+      },
+    },
+  };
+}
