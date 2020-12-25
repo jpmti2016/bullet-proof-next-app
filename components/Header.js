3c4e7eda8bb59ab8186eeca6c6039402f594ab19
Yampier Medina <jpmti2016@gmail.com>
2020-12-25 00:41:20 -0500

    add social auth

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,43 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/client";
 
-export default function Header () {
+export default function Header() {
+  const [session] = useSession();
+  console.log("SESSION", session);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signIn("github");
+  };
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    signOut();
+  };
+
   return (
-    <div className='header'>
-      <Link href='/'>
-        <a className='title'>My Blog</a>
+    <div className="header">
+      <Link href="/">
+        <a className="title">My Blog</a>
       </Link>
+      <div className="user-info">
+        {session ? (
+          <>
+            <img
+              src={session.user.github.avatar}
+              alt="user image"
+              className="user"
+            />
+            <a href="#" onClick={handleLogout} className="logout">
+              {" "}
+              Logout
+            </a>
+          </>
+        ) : (
+          <a href="#" onClick={handleLogin} className="logout">
+            Login
+          </a>
+        )}
+      </div>
     </div>
-  )
+  );
 }
